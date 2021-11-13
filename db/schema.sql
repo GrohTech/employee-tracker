553ed40d2ddab9ce56a82d809d382bf6a3778308
Lauren Groh <legroh@uwm.edu>
2021-11-13 17:38:40 -0600

    fix errors on schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,15 +16,15 @@ FOREIGN KEY (department_id)
 REFERENCES department(department_id) ON UPDATE CASCADE ON DELETE RESTRICT;
 );
 
-CREATE TABLE employee (
-id; INTEGER AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-CONSTRAINT fk_employeeRole_id
-FOREIGN KEY (employeeRole_id)
-REFERENCES employeeRole(employeeRole_id) ON UPDATE CASCADE,
-manager_id
-CONSTRAINT fk_manager_id
-FOREIGN KEY (manager_id)
-REFERENCES employee(manager_id) ON DELETE SET NULL;
-);
+-- CREATE TABLE employee (
+-- id; INTEGER AUTO_INCREMENT PRIMARY KEY,
+-- first_name VARCHAR(30) NOT NULL,
+-- last_name VARCHAR(30) NOT NULL,
+-- CONSTRAINT fk_employeeRole_id
+-- FOREIGN KEY (employeeRole_id)
+-- REFERENCES employeeRole(employeeRole_id) ON UPDATE CASCADE,
+-- manager_id
+-- CONSTRAINT fk_manager_id
+-- FOREIGN KEY (manager_id)
+-- REFERENCES employee(manager_id) ON DELETE SET NULL;
+-- );
