a752d48d1069a8976032ac435631f1056fe0453e
Lauren Groh <legroh@uwm.edu>
2021-11-14 13:41:47 -0600

    create file to house sql query functions, and express, start inquirer prompt

---
+++ db-queries.js
@@ -0,0 +1,24 @@
+const connection = require('./connection');
+
+class Db {
+constructor(connection){
+this.connection = connection;
+}
+
+findAllEmployees(){
+const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.dept_name, CONCAT(manager.first_name, " ", manager.last_name) AS manager
+FROM employee
+LEFT JOIN role
+ON employee.role_id = role.role_id
+LEFT JOIN department
+ON role.dept_id = department.dept_id
+LEFT JOIN employee manager
+ON manager.id = employee.manager_id
+`;
+    return  this.connection.promise().query(sql)
+}
+
+
+}
+
+module.exports = new Db(connection); 
--- server.js
+++ server.js
@@ -1,8 +1,43 @@
-const db = require('./db/connection');
+const db = require('./connection');
 const consoleTable = require('console.table');
+const dbQueries = require('./db-queries');
+const inquirer = require('inquirer');
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+function prompt() {
+    inquirer.prompt([{
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+            {name: 'View all employees',
+             value: 'view_employees'}
+            //  add more choices
+        ]
+
+    }]).then(res => {
+        switch(res.choice){
+            case 'view_employees':
+                viewEmployees()
+            break
+            // add more cases
+        }
+    })
+
+}
+
+function viewEmployees(){
+    dbQueries.findAllEmployees().then(([roles])=>{
+        console.log(roles);
+        console.table(roles);
+    }).then(()=>prompt())
+};
+
+prompt();
 
 // Start server after DB connection
 db.connect(err => {
